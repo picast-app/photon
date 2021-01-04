007745ffbcd818517f8ef2e08cfaaa23fa12496e
Mathis Bullinger <mathis@bullinger.dev>
2021-01-04 10:58:52 +0100

    feat: size in path & forward query string

--- src/handler.ts
+++ src/handler.ts
@@ -5,15 +5,30 @@ import download from './utils/download'
 import handleTask, { Task } from './task'
 
 export const onTheFly = async (event: APIGatewayEvent) => {
-  const url = event.requestContext.path?.replace(/^\//, '')
-  if (!/^https?:\/\//.test(url)) return { statusCode: 404 }
+  let url = event.requestContext.path?.replace(/^\//, '')
+  let size = 256
 
-  const data = await download(url)
+  if (/^\d+\//.test(url)) {
+    size = Math.min(parseInt(url.split('/')[0]), 2048)
+    url = url.replace(/^\d+\//, '')
+  }
+
+  if (!/^https?:\/\//.test(url)) return { statusCode: 400 }
+
+  const qStr =
+    event.queryStringParameters &&
+    Object.entries(event.queryStringParameters).length
+      ? `?${Object.entries(event.queryStringParameters)
+          .map(([k, v]) => `${k}=${v}`)
+          .join('&')}`
+      : ''
+
+  const data = await download(url + qStr)
 
   const format = new Headers(event.headers).get('accept')?.includes('webp')
     ? 'webp'
     : 'jpeg'
-  const size = parseInt(event.queryStringParameters?.size ?? '256')
+
   const out = await sharp(data)
     .resize(size, size, { withoutEnlargement: true })
     .toFormat(format)
@@ -24,6 +39,7 @@ export const onTheFly = async (event: APIGatewayEvent) => {
       statusCode: 200,
       headers: {
         'Content-Type': `image/${format}`,
+        'Cache-Control': 'public, max-age=2592000, immutable',
       },
       body: out.toString('base64'),
       isBase64Encoded: true,
