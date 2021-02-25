4414bd6972208fdc9640282569147ebd19586428
Mathis Bullinger <mathis@bullinger.dev>
2021-02-25 04:22:32 +0100

    feat: set cors response headers

--- src/handler.ts
+++ src/handler.ts
@@ -40,6 +40,7 @@ export const onTheFly = async (event: APIGatewayEvent) => {
       headers: {
         'Content-Type': `image/${format}`,
         'Cache-Control': 'public, max-age=2592000, immutable',
+        'Access-Control-Allow-Origin': 'https://picast.app',
       },
       body: out.toString('base64'),
       isBase64Encoded: true,
